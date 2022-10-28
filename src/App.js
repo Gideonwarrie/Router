09c04ce70490df243435e428f8d7693582ecec1a
Gideonwarrie <warriegideon101@gmail.com>
2022-10-28 20:56:53 +0100

    Routers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import useFetch from './useFetch';
 
 function App() {
+  const [page, setPage] = useState(1);
+  // const { loading, error, data } = useFetch(
+  //   `https://randomuser.me/api/?page=${page}&results=10&seed=abc`
+  // );
+   const { loading, error, data } = useFetch(
+     `https://randomuser.me/api/?results=1000&seed=abc`
+   );
+
+ 
+
+  
+  const PER_PAGE = 5;
+  
+  const total = data?.results?.length;
+  
+  const pages = 50;
+  
+  const skip = page * PER_PAGE - PER_PAGE;
+ 
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (!loading && error) {
+    return <>Error</>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="title"> USERS LIST</h1>
+     
+      {data?.results
+        .slice(skip, skip + PER_PAGE)
+         .slice((page - 1) * PER_PAGE, page * PER_PAGE)
+        .map((each, index) => {
+          const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
+          return (
+            <li key={name.toLowerCase().replaceAll(' ', '')}>{`${
+              index + 1
+            }.${name}`}</li>
+          );
+        })}
+      {
+        <button
+          disabled={page <= 1}
+          onClick={() => setPage((prev) => prev - 1)}
         >
-          Learn React
-        </a>
-      </header>
+          prev
+        </button>
+      }
+      <p className="pagination">
+        Pages: {page} of {pages}
+      </p>
+      {
+        <button
+          disabled={page >= pages}
+          aria-disabled={page >= pages}
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          next
+        </button>
+      }
+      
+      {Array.from({ length: pages }, (value, index) => index + 1).map(
+        (each) => (
+          <button onClick={() => setPage(each)}>{each}</button>
+        )
+      )}
     </div>
   );
 }
